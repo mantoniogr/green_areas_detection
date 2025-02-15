390d96efd8319c89907530503f76bef80cd0c1ac
Marco Garduño <mantoniogrx@gmail.com>
2025-02-14 23:27:31 -0600

    Improving code.

--- hSelection.py
+++ hSelection.py
@@ -1,5 +1,12 @@
+"""
+This script analyzes the Hue values in a selected region of interest (ROI) 
+from an image in HSV color space. It extracts min/max Hue values and saves
+the HSV channels as separate images.
+"""
+
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Use Agg backend for headless environments
 import matplotlib
@@ -8,6 +15,8 @@ matplotlib.use('Agg')
 # Image
 imgName = 'images/lena.png'
 img = cv2.imread(imgName)
+if img is None:
+    raise FileNotFoundError(f"Could not load image: {imgName}")
 
 # Convert BGR to RGB (matplotlib expects RGB format)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -18,7 +27,7 @@ x2 = 230
 y1 = 200
 y2 = 230
 
-# Display the roi using matplotlib
+# Draw ROI rectangle 
 cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (255, 0, 0), 2)
 plt.imshow(img_rgb)
 plt.title("RGB image")
@@ -31,24 +40,22 @@ imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 # Extract ROI Hue values
 roi_hue = imgHSV[y1:y2, x1:x2, 0]
 
-# Display the image using matplotlib
-plt.imshow(imgHSV)
-plt.title("HSV image")
-plt.axis("off")  # Hide axes for a cleaner display
-plt.savefig("images/imageHSV.png")  # Save the plot to a file
+# Save HSV image and channels
+HSV_HUE_MAX = 179  # OpenCV uses 0-179 for Hue
+RGB_MAX = 255
+
+h, s, v = cv2.split(imgHSV)
+h_scaled = (h * RGB_MAX / HSV_HUE_MAX).astype("uint8")
+imgHSV_scaled = cv2.merge([h_scaled, s, v])
+cv2.imwrite("hsv_image_scaled.png", imgHSV_scaled)
+cv2.imwrite("hue_channel.png", h_scaled)
+cv2.imwrite("saturation_channel.png", s)
+cv2.imwrite("value_channel.png", v)
 
 # Initialize min and max
-min = 360
-max = 0
-
-# Get min and max from image section
-for j in range(y1, y2):
-   for i in range(x1, x2):
-       if imgHSV[j,i,0] > max:
-           max = imgHSV[j,i,0]
-       if imgHSV[j,i,0] < min:
-           min = imgHSV[j,i,0]
+min_hue = np.min(roi_hue)
+max_hue = np.max(roi_hue)
 
 # Print min and max
-print("Min value: " + str(min))
-print("Max value: " + str(max))
+print("Min value: " + str(min_hue))
+print("Max value: " + str(max_hue))
